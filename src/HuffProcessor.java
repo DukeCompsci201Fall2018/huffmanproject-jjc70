bac96e272a672db5ce9ff1960a2407ba08355438
Januario Carreiro <jjc70@duke.edu>
2018-12-05 20:14:18 -0500

    Decompress Finished, Compress Started

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -42,13 +42,43 @@ public class HuffProcessor {
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
 	}
+
+	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
+
+	}
+
+	private HuffNode makeTreeFromCounts(int counts) {
+		return null;
+	}
+
+	private String[] makeCodingsFromTree(HuffNode root) {
+		return null;
+	}
+
+	private void writeHeader(HuffNode root, BitOutputStream out) {
+
+	}
+
+	private int[] readForCounts(BitInputStream in) {
+		int[] freq = new int[ALPH_SIZE + 1];
+		while(in.readBits(BITS_PER_WORD) != PSEUDO_EOF) {
+			freq[in.readBits(BITS_PER_WORD)] = freq[in.readBits(BITS_PER_WORD)] + 1;
+		}
+		freq[PSEUDO_EOF] = 1;
+		return freq;
+	}
+
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -60,11 +90,50 @@ public class HuffProcessor {
 	 */
 	public void decompress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+		int bits = in.readBits(BITS_PER_INT);
+		if (bits != HUFF_TREE) {
+			throw new HuffException("illegal header starts with " + bits);
 		}
+
+		HuffNode root = readTreeHeader(in);
+		readCompressedBits(root, in, out);
 		out.close();
+	}
+
+	private HuffNode readTreeHeader(BitInputStream in) {
+		int bit = in.readBits(1);
+		if (bit == -1) throw new RuntimeException("Illegal argument: numBits must be on [1, 32]");
+		if (bit == 0) {
+			HuffNode left = readTreeHeader(in);
+			HuffNode right = readTreeHeader(in);
+			return new HuffNode(0, 0, left, right);
+		}
+		else {
+			int value = in.readBits(9);
+			return new HuffNode(value, 0, null , null);
+		}
+	}
+
+	private void readCompressedBits(HuffNode root, BitInputStream in, BitOutputStream out) {
+		HuffNode current = root;
+		while (true) {
+			int bits = in.readBits(1);
+			if (bits == -1) throw new HuffException("bad input, no PSEUDO_EOF");
+			else {
+				if (bits == 0) current = current.myLeft;
+				else current = current.myRight;
+
+				if (current.myLeft == null && current.myRight == null) {
+					if (current.myValue == PSEUDO_EOF) {
+						break;
+					}
+					else {
+						out.writeBits(BITS_PER_WORD, current.myValue);
+						current = root;
+					}
+				}
+			}
+
+		}
 	}
 }
